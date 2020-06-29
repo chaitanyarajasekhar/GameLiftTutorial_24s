99c8199798a8a409bc4da693da04dd3aebb904dc
chaitanyar56 <chaitanyar5atp@gmail.com>
2020-06-29 12:30:44 +0530

    Added Antize VOIP plugin - Not working with gamelift - Client packagebuild crashes

--- Source/GameLiftTutorial/Private/GameLiftTutorialGameMode.cpp
+++ Source/GameLiftTutorial/Private/GameLiftTutorialGameMode.cpp
@@ -7,7 +7,8 @@
 AGameLiftTutorialGameMode::AGameLiftTutorialGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/AntizeVOIPSystem/Blueprints/BP_VOIPCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
